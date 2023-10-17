0aa75f8d8903d83ba2581db458a3a517ac27875d
zapper163 <zapingero163@gmail.com>
2023-10-17 16:56:34 +0200

    Update Client.cs

--- Assets/Scripts/Menus/Main/Client.cs
+++ Assets/Scripts/Menus/Main/Client.cs
@@ -12,6 +12,7 @@ using System.Linq;
 
 public class Client : MonoBehaviour
 {
+    //TCP
     Socket listen;
     IPEndPoint connect;
 
@@ -21,6 +22,12 @@ public class Client : MonoBehaviour
     bool checkingJoin;
 
     [SerializeField] GameObject fadeIn;
+
+    //UDP
+    Socket listenUdp;
+    IPEndPoint endPoint;
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -57,7 +64,34 @@ public class Client : MonoBehaviour
     public void CreateUdpClient()
     {
         
+        endPoint = new IPEndPoint(IPAddress.Parse(ipField.text), 6000);
+
+        listenUdp = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        Debug.Log("Ip: " + ipField.text);
+
+        //Esto va aqui???
+        //listen.Connect(connect);
+
+
+        byte[] data = new byte[1024];
+        string input, stringData;
+        string welcome = "Hello, are you there?";
+        data = Encoding.ASCII.GetBytes(welcome);
+        listenUdp.SendTo(data, data.Length, SocketFlags.None, endPoint);
+
+        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+        EndPoint Remote = (EndPoint)sender;
+
+        data = new byte[1024];
+        int recv = listenUdp.ReceiveFrom(data, ref Remote);
+
+        //Console.WriteLine("Message received from {0}:", Remote.ToString());
+        //Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+
+        StartCoroutine(JoinRoom());
+
     }
+
 
     IEnumerator JoinRoom()
     {
